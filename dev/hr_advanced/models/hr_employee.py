eac26a044021ca80016f6c571074af069f9a3fec
phungduy2k2 <131169758+phungduy2k2@users.noreply.github.com>
2025-03-29 11:32:09 +0700

    hr advance v2 (#10)
    Thêm wizard, smartbutton, liên kết giữa certification và skill

--- dev/hr_advanced/models/hr_employee.py
+++ dev/hr_advanced/models/hr_employee.py
@@ -12,12 +12,16 @@ class HREmployeeInherit(models.Model):
         'employee_id',
         'certification_id',
         string='Chứng chỉ')
+    certification_count = fields.Integer(compute='_compute_certification_count', string='Số chứng chỉ')
     skill_ids = fields.Many2many(
         'hr.employee.skill',
         'employee_skill_rel',
         'employee_id',
         'skill_id',
         string='Kỹ năng')
+    skill_count = fields.Integer(compute='_compute_skill_count', string='Số kỹ năng')
+    has_certification = fields.Boolean('Has certification', compute='_compute_has_certification')
+    is_manager = fields.Boolean('Is manager', compute='_compute_is_manager')
 
     @api.depends('certification_ids', 'skill_ids')
     def _compute_years_of_experience(self):
@@ -26,16 +30,31 @@ class HREmployeeInherit(models.Model):
             skill_years = sum(skill.years_of_experience for skill in rec.skill_ids)
             rec.years_of_experience = cert_years + skill_years
 
+    def _compute_certification_count(self):
+        for employee in self:
+            employee.certification_count = len(employee.certification_ids) or 0
+
+    def _compute_skill_count(self):
+        for employee in self:
+            employee.skill_count = len(employee.skill_ids) or 0
+
+    @api.depends('certification_ids')
+    def _compute_has_certification(self):
+        for rec in self:
+            rec.has_certification = bool(rec.certification_ids)
+
+    def _compute_is_manager(self):
+        for rec in self:
+            rec.is_manager = (self.env.user.has_group('hr_advanced.group_hr_certification_manager') or
+                            self.env.user.has_group('hr_advanced.group_hr_skill_manager'))
+
     def action_view_certifications(self):
         return {
             'type': 'ir.actions.act_window',
             'name': 'Chứng chỉ nhân viên',
             'res_model': 'hr.employee.certification',
             'view_mode': 'tree,form',
-            'domain': [('employee_id', '=', self.id)],
-            'context': {
-                'default_employee_id': self.id
-            }
+            'domain': [('id', 'in', self.certification_ids.ids)],
         }
 
     def action_view_skills(self):
@@ -44,8 +63,5 @@ class HREmployeeInherit(models.Model):
             'name': 'Kỹ năng nhân viên',
             'res_model': 'hr.employee.skill',
             'view_mode': 'tree,form',
-            'domain': [('employee_id', '=', self.id)],
-            'context': {
-                'default_employee_id': self.id
-            }
+            'domain': [('id', 'in', self.skill_ids.ids)],
         }
